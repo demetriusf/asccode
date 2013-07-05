20ad8f48b41ed6dd4f5d9fc317177dab2f05f9bd
Demetrius Feijóo Campos <demetrius@MacBook-Pro-Demetrius.local>
2013-07-04 23:50:49 -0300

    fazendo topo 20%

--- application/views/home.php
+++ application/views/home.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html lang="pt-br" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="pt-br" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="pt-br" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="pt-br" class="no-js"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -10,18 +10,52 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
-        <link rel="stylesheet" href="css/normalize.min.css">
         <link rel="stylesheet" href="css/main.css">
 
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+            <p class="chromeframe">Você está usando um navegador <strong>desatualizado</strong>. Por favor <a href="http://browsehappy.com/">atualize o seu navegador</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">ative Google Chrome Frame</a> para melhorar a sua experiência.</p>
         <![endif]-->
 
-        <p>Hello world! This is HTML5 Boilerplate.</p>
-
+    	<header>
+    	
+    		<div class="box-centered box-main-width" >
+    		
+    		    <div class="header-wrapper-box box-centered" >
+    		
+    		
+	    			<h1 class="header-logo-box"><a href="http://www.google.com.br" title="Ir para inicial da Asccode" class="logo ir" >Asccode desenvolvimento de sistemas</a></h1>
+	    			<nav class="header-nav-box">
+	    			
+	    				<ul class="nav-list clear-fix">
+	    					<li class="nav-list-item"> <a href="http://www.google.com.br" title="" >Home</a> </li>
+	    					<li class="nav-list-item"> <a href="http://www.google.com.br" title="" >Quem Somos</a> </li>
+	    					<li class="nav-list-item"> <a href="http://www.google.com.br" title="" >O que fazemos</a> </li>
+	    					<li class="nav-list-item"> <a href="http://www.google.com.br" title="" >Clientes</a> </li>
+	    					<li class="nav-list-item"> <a href="http://www.google.com.br" title="" >Cases</a> </li>
+	    					<li class="nav-list-item"> <a href="http://www.google.com.br" title="" >Contato</a> </li>
+	    				</ul>
+	    			
+	    			</nav>
+	    			
+	    			<ul class="header-social-box social-list">
+	    					<li class="social-list-item"> <a href="http://www.google.com.br" title="" class="facebook">F</a> </li>
+	    					<li class="social-list-item"> <a href="http://www.google.com.br" title="" class="twitter">T</a> </li>
+	    					<li class="social-list-item"> <a href="http://www.google.com.br" title="" class="gplus">G</a> </li>
+	    			</ul>
+    		
+    			</div>
+    			
+    		</div>
+    	
+    	</header>
+    	
+    	<footer>
+    	
+    	</footer>
+    
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
